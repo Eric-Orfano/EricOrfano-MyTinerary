1f4532892d6cf6f67a6a3057e884993b39b3b302
Eric-Orfano <orfano.eric@gmail.com>
2024-03-17 10:51:18 -0300

    Correccion de <a>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
     <>
         <footer className="flex justify-around items-center border border-solid bg-gray-900 text-white">
             <div className="flex flex-row flex-wrap m-2 p-5">
-                <a className="flex flex-row items-center gap-2 font-bold p-10" href="https://web.whatsapp.com/"><img className="w-6 h-6" src="./whatsapp.png" alt="" />WhatsApp</a>
-                <a className="flex flex-row items-center gap-2 font-bold p-10" href="https://instagram.com"><img className="w-6 h-6" src="./instagram.png" alt="" /> Instagram</a>
-                <a className="flex flex-row items-center gap-2 font-bold p-10" href="https://facebook.com"><img className="w-6 h-6" src="./facebook.png" alt="" />Facebook</a>
-                <a className="flex flex-row items-center gap-2 font-bold p-10" href="https://twitter.com"><img className="w-6 h-6" src="./twitter.png" alt="" />Twitter</a>
-                <a className="flex flex-row items-center gap-2 font-bold p-10" href="https://hotmail.com"><img className="w-6 h-6" src="./gmail.png" alt="" /><a href="https://gmail.com">Gmail</a></a>
+                <Link className="flex flex-row items-center gap-2 font-bold p-10" to="https://web.whatsapp.com/"><img className="w-6 h-6" src="./whatsapp.png" alt="" />WhatsApp</Link>
+                <Link className="flex flex-row items-center gap-2 font-bold p-10" to="https://instagram.com"><img className="w-6 h-6" src="./instagram.png" alt="" /> Instagram</Link>
+                <Link className="flex flex-row items-center gap-2 font-bold p-10" to="https://facebook.com"><img className="w-6 h-6" src="./facebook.png" alt="" />Facebook</Link>
+                <Link className="flex flex-row items-center gap-2 font-bold p-10" to="https://twitter.com"><img className="w-6 h-6" src="./twitter.png" alt="" />Twitter</Link>
+                <Link className="flex flex-row items-center gap-2 font-bold p-10" to="https://gmail.com"><img className="w-6 h-6" src="./gmail.png" alt="" />Gmail</Link>
             </div>
         </footer>   
     </>
