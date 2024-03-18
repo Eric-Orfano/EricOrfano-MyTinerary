cd13e58fd53a067f00814f2bf9fecd1e58fa9fa1
Eric-Orfano <orfano.eric@gmail.com>
2024-03-18 10:09:18 -0300

    Estructura de Itinerario

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -9,20 +9,20 @@ function Itinerary({itinerario}) {
         return final
       }
   return (
-        <div className=' flex flex-col flex-wrap my-5 bg-gray-950 text-gray-200 items-center'>
-          <h2 className='text-center text-xl font-semibold py-2 underline md:text-2xl xl:text-5xl'>{itinerario.title}</h2>
-          <section className='self-center w-2/4 py-5 flex flex-col'><img className='rounded-full md:w-[256px] self-center' src={itinerario.guide_image} alt={itinerario.guide} />
-          <h5 className='text-center text-xl font-semibold xl:text-3xl'>{itinerario.guide}</h5></section>
-          <p className='text-lg pb-5 md:text-xl xl:text-2xl lg:w-[80ch]'>{itinerario.description}</p>
-          <p className='text-center font-semibold text-xl'>Price: {precio(itinerario.price)}</p>
-          <p className='text-center font-semibold text-xl'>Duration: {itinerario.duration} Hours</p>
+        <div className=' '>
+          <h2 className=''>{itinerario.title}</h2>
+          <section className=''><img className='' src={itinerario.guide_image} alt={itinerario.guide} />
+          <h4 className="">{itinerario.guide}</h4></section>
+          <p className=''>{itinerario.description}</p>
+          <p className=''>Price: {precio(itinerario.price)}</p>
+          <p className=''>Duration: {itinerario.duration} Hours</p>
           <section>
-            <h3 className='text-center font-semibold text-xl underline py-5'>ACTIVITIES</h3>
-            <div className='flex flex-col items-center'>
-              {itinerario.activities.map((e) => <img key={Math.random()} className='w-full my-2 lg:w-[1024px]' src={e}></img>)}
+            <h3 className=''>ACTIVITIES</h3>
+            <div className=''>
+              {itinerario.activities.map((e) => <img key={Math.random()} className='' src={e}></img>)}
             </div>
           </section>
-          <p className='text-lg p-3 font-semibold md:texl-xl'>{itinerario.hashtags.join(" ")}</p>
+          <p className=''>{itinerario.hashtags.join(" ")}</p>
         </div>
   )
 }
